dccc78406e1067db2d51c64f657b84c4b2733b88
seroton11n <linara.fatkullina.04@mail.ru>
2024-10-15 00:50:24 +0500

    Update 11.py

--- examples/11.py
+++ examples/11.py
@@ -1,4 +1,5 @@
+def book_list(books, func):
+    for book in books:
+        print(func(book))
 books = ['System Design','Python и DevOps','Git. Практическое руководство']
-def book_print(book):
-    return book.upper() + ' - прочитано'
-book_list(books, book_print)
+book_list(books, lambda book: book.upper() + ' - прочитано')
